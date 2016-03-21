547359b344dd67bc97319e1c704d21d4c0b28ed1
Evgeniy Melkov <jeka.mel@gmail.com>
2016-03-21 13:11:12 +0200

    Base project

---
+++ FreeCropImageTool/FreeCropImageTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreeCropImageTool
+//
+//  Created by Evgeniy Melkov on 21.03.16.
+//  Copyright © 2016 Evgeniy Melkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
